95f68ffb6b667da35735e56813ccff722096e64a
Mojtaba Jahani <moja987@gmail.com>
2022-06-06 19:26:20 -0700

    76 core, bash as shell

--- EDTA.sh
+++ EDTA.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source conda activate EDTA
+eval "$(command conda 'shell.bash' 'hook' 2>/dev/null)"
+conda activate EDTA
 FASTA=$1 #/DATA/home/mjahani/curation_AGA10/AGA10hap1.reviewed.chr_assembled.fasta
 SAVEDIR=$2
 
@@ -13,7 +14,7 @@ while read SCAFFOLD; do
     seqtk subseq $FASTA ${SAVEDIR}/${SCAFFOLD}.txt >${SAVEDIR}/${FASTA_BASE%%.fasta}/${SCAFFOLD}/${FASTA_BASE%%.fasta}_${SCAFFOLD}.fatsa
     rm ${SAVEDIR}/${SCAFFOLD}.txt
     cd ${SAVEDIR}/${FASTA_BASE%%.fasta}/${SCAFFOLD}
-    singularity exec /DATA/home/mjahani/bin/EDTA/EDTA.sif EDTA.pl --overwrite 0 --genome ${SAVEDIR}/${FASTA_BASE%%.fasta}/${SCAFFOLD}/${FASTA_BASE%%.fasta}_${SCAFFOLD}.fatsa --sensitive 0 --anno 1 --evaluate 0 --threads 152
+    singularity exec /DATA/home/mjahani/bin/EDTA/EDTA.sif EDTA.pl --overwrite 0 --genome ${SAVEDIR}/${FASTA_BASE%%.fasta}/${SCAFFOLD}/${FASTA_BASE%%.fasta}_${SCAFFOLD}.fatsa --sensitive 0 --anno 1 --evaluate 0 --threads 76
     awk -v var=$SCAFFOLD '{print var,"\t",$3,"\t",$4,"\t",$5}' ${SAVEDIR}/${FASTA_BASE%%.fasta}/${SCAFFOLD}/${FASTA_BASE%%.fasta}_${SCAFFOLD}.fatsa.mod.EDTA.TEanno.gff3 >>${SAVEDIR}/${FASTA_BASE%%.fasta}_10chr.bed
 done <${SAVEDIR}/${FASTA_BASE%%.fasta}_CHR_LIST
 
