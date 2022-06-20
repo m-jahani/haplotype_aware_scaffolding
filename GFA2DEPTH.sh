2f1d962a9cf54e5b5bd38c9e4545542aeedb39ae
Mojtaba Jahani <moja987@gmail.com>
2022-06-20 11:00:33 -0700

    added save directory to final result

--- GFA2DEPTH.sh
+++ GFA2DEPTH.sh
@@ -5,5 +5,5 @@ awk '/^A/{print $2,"\t",$3,"\t",$3+$7,"\t",$8}' $GFA | sed 's/ //g' >${GFA}_READ
 bedtools sort -i ${GFA}_READ.bed >${GFA}_READ.sorted.bed
 rm ${GFA}_READ.bed
 awk '/^S/{print $2,"\t",$4}' $GFA | sed 's/LN:i://g' | sed 's/ //g' >${GFA}_contigs
-bedtools genomecov -i ${GFA}_READ.sorted.bed -g ${GFA}_contigs -bga >${SAVE_DIR}/${GFA%%.gfa}.depth
+bedtools genomecov -i ${GFA}_READ.sorted.bed -g ${GFA}_contigs -bga >${SAVE_DIR}/$(basename ${GFA%%.gfa}.depth)
 rm ${GFA}_READ.sorted.bed ${GFA}_contigs
