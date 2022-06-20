e5c18c465f8d58f26088f797edc8cd9092455c06
Mojtaba Jahani <moja987@gmail.com>
2022-06-20 10:26:54 -0700

    add directory to recombination.R input

--- contig_info.sh
+++ contig_info.sh
@@ -14,10 +14,10 @@ SAVE_DIR=$8     #/DATA/home/mjahani/curation_AGA10/TEMP_TEST/result
 
 [ -d ${SAVE_DIR}/LINKAGE_MAP ] || mkdir -p ${SAVE_DIR}/LINKAGE_MAP
 bash MAPLINKAGE.sh $RAW_CONTIGS_HAP1 $prime_3 $prime_5 ${SAVE_DIR}/LINKAGE_MAP
-Rscript recombination.R ${SAVE_DIR}/$(basename "${prime_3%%.fasta}")_$(basename "${RAW_CONTIGS_HAP1%%.fasta}.sam") ${SAVE_DIR}/$(basename "${prime_5%%.fasta}")_$(basename "${RAW_CONTIGS_HAP1%%.fasta}.sam") $LINKAGE_DATA ${SAVE_DIR}/LINKAGE_MAP
+Rscript recombination.R ${SAVE_DIR}/LINKAGE_MAP/$(basename "${prime_3%%.fasta}")_$(basename "${RAW_CONTIGS_HAP1%%.fasta}.sam") ${SAVE_DIR}/LINKAGE_MAP/$(basename "${prime_5%%.fasta}")_$(basename "${RAW_CONTIGS_HAP1%%.fasta}.sam") $LINKAGE_DATA ${SAVE_DIR}/LINKAGE_MAP
 
 bash MAPLINKAGE.sh $RAW_CONTIGS_HAP2 $prime_3 $prime_5 ${SAVE_DIR}/LINKAGE_MAP
-Rscript recombination.R ${SAVE_DIR}/$(basename "${prime_3%%.fasta}")_$(basename "${RAW_CONTIGS_HAP2%%.fasta}.sam") ${SAVE_DIR}/$(basename "${prime_5%%.fasta}")_$(basename "${RAW_CONTIGS_HAP2%%.fasta}.sam") $LINKAGE_DATA ${SAVE_DIR}/LINKAGE_MAP
+Rscript recombination.R ${SAVE_DIR}/LINKAGE_MAP/$(basename "${prime_3%%.fasta}")_$(basename "${RAW_CONTIGS_HAP2%%.fasta}.sam") ${SAVE_DIR}/LINKAGE_MAP/$(basename "${prime_5%%.fasta}")_$(basename "${RAW_CONTIGS_HAP2%%.fasta}.sam") $LINKAGE_DATA ${SAVE_DIR}/LINKAGE_MAP
 
 [ -d ${SAVE_DIR}/TELOMERE ] || mkdir -p ${SAVE_DIR}/TELOMERE
 bash TELOMERE.sh $RAW_CONTIGS_HAP1 200000 $(basename ${RAW_CONTIGS_HAP1%.fasta}) ${SAVE_DIR}/TELOMERE
@@ -27,9 +27,9 @@ bash TELOMERE.sh $RAW_CONTIGS_HAP2 200000 $(basename ${RAW_CONTIGS_HAP2%.fasta})
 bash GFA2DEPTH.sh $RAW_CONTIGS_HAP1_GRAPH ${SAVE_DIR}/DEPTH
 bash GFA2DEPTH.sh $RAW_CONTIGS_HAP2_GRAPH ${SAVE_DIR}/DEPTH
 
-[ -d ${SAVE_DIR}/EDTA_LTR ] || mkdir -p ${SAVE_DIR}/EDTA_LTR
-bash EDTA.sh $RAW_CONTIGS_HAP1 ${SAVE_DIR}/EDTA_LTR &
-bash EDTA.sh $RAW_CONTIGS_HAP2 ${SAVE_DIR}/EDTA_LTR
+#[ -d ${SAVE_DIR}/EDTA_LTR ] || mkdir -p ${SAVE_DIR}/EDTA_LTR
+#bash EDTA.sh $RAW_CONTIGS_HAP1 ${SAVE_DIR}/EDTA_LTR &
+#bash EDTA.sh $RAW_CONTIGS_HAP2 ${SAVE_DIR}/EDTA_LTR
 
 [ -d ${SAVE_DIR}/SYNTENY ] || mkdir -p ${SAVE_DIR}/SYNTENY
 bash minimap.sh $RAW_CONTIGS_HAP1 $RAW_CONTIGS_HAP2 ${SAVE_DIR}/SYNTENY
