d98326bb1638b5715bdbda2e88882c3443997bb0
Mojtaba Jahani <moja987@gmail.com>
2022-06-24 12:17:10 -0700

    error in in naming result file resolved

--- minimap.sh
+++ minimap.sh
@@ -5,7 +5,5 @@ conda activate scaff_env
 ASSEMBLY1=$1 #/DATA/home/mjahani/opt/juicer/scaffolding/AGA10_hap1/3D_DNA/AGA10.hic.hap1.p_ctg.final.fasta
 ASSEMBLY2=$2 #/DATA/home/mjahani/opt/juicer/scaffolding/AGA10_hap2/3D_DNA/AGA10.hic.hap2.p_ctg.final.fasta
 SAVE_DIR=$3  #/DATA/home/mjahani/curation_AGA10
-ASSEMBLY1_FILE=${ASSEMBLY1##*/}
-ASSEMBLY2_FILE=${ASSEMBLY2##*/}
 
-/DATA/home/mjahani/bin/minimap2/minimap2 -x asm10 -t 152 $ASSEMBLY1 $ASSEMBLY2 >${SAVE_DIR}/$(basename "${RAW_CONTIGS_HAP1%%.fasta}")_$(basename "${RAW_CONTIGS_HAP2%%.fasta}").paf
+/DATA/home/mjahani/bin/minimap2/minimap2 -x asm10 -t 152 $ASSEMBLY1 $ASSEMBLY2 >${SAVE_DIR}/$(basename "${ASSEMBLY1%%.fasta}")_$(basename "${ASSEMBLY2%%.fasta}").paf
