eaa42645789bed6909117c8053cad57e1bcbfe20
Mojtaba Jahani <moja987@gmail.com>
2022-09-03 16:49:14 -0700

    edit order of chromosomes

--- mix_reviewed_assemblies.R
+++ mix_reviewed_assemblies.R
@@ -2,16 +2,17 @@ library(data.table)
 library(tidyverse)
 
 
-args = commandArgs(trailingOnly = TRUE)
+#args = commandArgs(trailingOnly = TRUE)
 
 ASSEM1 <- args[1] #reviewed assembly hap1 #"/Users/mojtabajahani/Downloads/AGA10_r0/AGA10.hic.hap1.p_ctg.0.assembly"
 ASSEM2 <- args[2] #reviewed assembly hap2 #"/Users/mojtabajahani/Downloads/AGA10_r0/AGA10.hic.hap2.p_ctg.0.assembly"
 map_hap12 <- args[3] #read minimap2 alignment between hap1 and hap2 contigs in paf format #"/Users/mojtabajahani/Downloads/AGA10_r0/AGA10_H12.paf"
 save_dir <- args[4]
-# ASSEM1 <- "/Users/mojtabajahani/Downloads/AGA10_r0/new_30_june_2022/AGA10.hic.hap1.p_ctg.0.review.assembly"
-# ASSEM2 <- "/Users/mojtabajahani/Downloads/AGA10_r0/new_30_june_2022/AGA10.hic.hap2.p_ctg.0.review.assembly"
-# map_hap12 <- "/Users/mojtabajahani/Downloads/AGA10_r0/new_30_june_2022/AGA10.hic.hap1_AGA10.hic.hap2.paf"
-# save_dir <- "/Users/mojtabajahani/Downloads/AGA10_r0/new_30_june_2022"
+ 
+# ASSEM1 <- "/Users/mojtabajahani/Downloads/AGA10_r0/new_30_june_2022/MK-ULTRA/MK_ultra.hic.hap1.p_ctg.0.review.assembly"
+# ASSEM2 <- "/Users/mojtabajahani/Downloads/AGA10_r0/new_30_june_2022/MK-ULTRA/MK_ultra.hic.hap2.p_ctg.0.review.assembly"
+# map_hap12 <- "/Users/mojtabajahani/Downloads/AGA10_r0/new_30_june_2022/MK-ULTRA/MK_ultra.hic.hap1.p_ctg_MK_ultra.hic.hap2.p_ctg.paf"
+# save_dir <- "/Users/mojtabajahani/Downloads/AGA10_r0/new_30_june_2022/MK-ULTRA"
 ####################################################################Read Data#####################################################################   
 # read *reviewed assembly hap1
 read.table(ASSEM1,
@@ -30,7 +31,7 @@ read.table(map_hap12,
 ##################################################Contigs/fragment/Debris order in assembly file####################################################          
 
 #calculate the original length of fragmented contigs in Hap1 (for sorting) 
-HAP1 %>%
+HAP1 %>% 
   filter(grepl("^>",V1)) %>% 
   select(fragment_ID = V1,
          original_order = V2,
@@ -40,7 +41,7 @@ HAP1 %>%
            sep = ":::",
            remove=F)  %>% 
   group_by(contig) %>% 
-  summarize(total_length=sum(length)) %>%
+  summarize(total_length=sum(length)) %>% 
   ungroup() -> CONTIG_LENGTH_HAP1
 
 #prepare contig data for hap 1    
@@ -48,14 +49,14 @@ HAP1 %>%
   filter(grepl("^>",V1)) %>% 
   select(fragment_ID = V1,
          original_order = V2,
-         length = V3)  %>%
+         length = V3)  %>% 
   separate(fragment_ID, 
            into = c("contig","lable1","lable2"),
            sep = ":::",
            remove=F) %>% 
   full_join(.,
-            CONTIG_LENGTH_HAP1) %>%
-  mutate(HAP = 1) %>%
+            CONTIG_LENGTH_HAP1) %>% 
+  mutate(HAP = 1) %>% 
   select(fragment_ID,
          original_order,
          length,
@@ -101,10 +102,10 @@ rm(CONTIG_LENGTH_HAP2)
 
 #hap1 and hap 2 contigs with the new order
 rbind(DATA_HAP1,
-      DATA_HAP2) %>%
+      DATA_HAP2) %>% 
   arrange(
-    desc(total_length)) %>%
-  mutate(new_order = row_number()) %>%
+    desc(total_length)) %>% 
+  mutate(new_order = row_number()) %>% 
   select(fragment_ID,
          new_order,
          length) -> HAP1_HAP2_CONTOGS_NEW_ORDER
@@ -113,7 +114,7 @@ rbind(DATA_HAP1,
 rbind(DATA_HAP1,
       DATA_HAP2) %>%
   arrange(
-    desc(total_length)) %>%
+    desc(total_length)) %>% 
   mutate(new_order = row_number()) %>% 
   select(HAP,
          original_order,
@@ -125,8 +126,8 @@ HAP1 %>%
   mutate(CHR = row_number()) %>% 
   gather(contig_order_in_chr, original_order_sign, V1:V10000) %>% 
   filter(!is.na(original_order_sign)) %>% 
-  mutate(contig_order_in_chr=as.numeric(gsub("V","",contig_order_in_chr)))  %>%
-  mutate(original_order = abs(as.numeric(original_order_sign))) %>%
+  mutate(contig_order_in_chr=as.numeric(gsub("V","",contig_order_in_chr))) %>% 
+  mutate(original_order = abs(as.numeric(original_order_sign))) %>% 
   arrange(CHR,
           contig_order_in_chr) %>% 
   mutate(HAP = 1) -> CHR_CONTIG_HAP1_INFO
@@ -178,8 +179,8 @@ rm(CHR_LENGTH_HAP1,
 
 #######################################################Chromosome order in contact map#########################################################
 #corresponding contigs in hap1 
-HAP1_HAP2_ALIGNMENT %>%
-  mutate(alignment_perc = V11/V2) %>%
+HAP1_HAP2_ALIGNMENT %>% 
+  mutate(alignment_perc = V11/V2) %>% 
   filter(V12 > 1) %>%
   group_by(V1) %>%
   filter(alignment_perc == max(alignment_perc)) %>%
@@ -245,9 +246,9 @@ rbind(CHR_CONTIG_HAP1_INFO,
       CHR_CONTIG_HAP2_INFO)  %>%
   select(CHR,
          original_order,
-         HAP) %>%
+         HAP) %>% 
   full_join(.,
-            ORDER) %>%
+            ORDER) %>% 
   select(-original_order) %>%
   full_join(.,
             CONTIG_COR) -> DATA_CHR_PAIR
@@ -259,37 +260,37 @@ DATA_CHR_PAIR %>% filter(HAP==2) %>% select(CHR_HAP2 = CHR, new_order_hap2 = new
 
 rm(DATA_CHR_PAIR)
 
- full_join(DATA_CHR_PAIR_HAP1,DATA_CHR_PAIR_HAP2) %>%
+ full_join(DATA_CHR_PAIR_HAP1,DATA_CHR_PAIR_HAP2) %>% 
    group_by(CHR_HAP1,CHR_HAP2) %>%
    tally() %>%
-   ungroup() %>%
+   ungroup() %>% 
    filter(!is.na(CHR_HAP1),
-          !is.na(CHR_HAP2)) %>%
-   arrange(desc(n)) %>%
+          !is.na(CHR_HAP2)) %>% 
+   arrange(desc(n)) %>% 
    left_join(.,
-             select(filter(CHR_LENGTH_HAP12,HAP == 1), CHR_HAP1 = CHR ,CHR_length_HAP1 = CHR_length)) %>%
+             select(filter(CHR_LENGTH_HAP12,HAP == 1), CHR_HAP1 = CHR ,CHR_length_HAP1 = CHR_length)) %>% 
    left_join(.,
-             select(filter(CHR_LENGTH_HAP12,HAP == 2), CHR_HAP2 = CHR ,CHR_length_HAP2 = CHR_length)) %>%
-   mutate(length_diff_kb = abs(CHR_length_HAP2-CHR_length_HAP1)/1000) %>%
-   filter(CHR_length_HAP1 > 10000000, CHR_length_HAP2 > 10000000) %>%
+             select(filter(CHR_LENGTH_HAP12,HAP == 2), CHR_HAP2 = CHR ,CHR_length_HAP2 = CHR_length)) %>% 
+   mutate(length_diff_kb = abs(CHR_length_HAP2-CHR_length_HAP1)/1000) %>% 
+   filter(CHR_length_HAP1 > 10000000, CHR_length_HAP2 > 10000000) %>% 
    group_by(CHR_HAP1) %>%
    filter(length_diff_kb == min(length_diff_kb)) %>%
    ungroup() %>%
    group_by(CHR_HAP2) %>%
    filter(length_diff_kb == min(length_diff_kb)) %>%
-   ungroup() %>%
+   ungroup() %>%  
    group_by(CHR_HAP1) %>%
-   filter(n == min(n)) %>%
+   filter(n == max(n)) %>%
    ungroup() %>%
    group_by(CHR_HAP2) %>%
-   filter(n == min(n)) %>%
-   ungroup() %>%
-   arrange(CHR_length_HAP1)  %>%
+   filter(n == max(n)) %>%
+   ungroup() %>% 
+   arrange(desc(CHR_length_HAP1)) %>% 
    select(CHR_HAP1,
-          CHR_HAP2) %>%
-   mutate(chr_ord = row_number()) %>%
+          CHR_HAP2) %>% 
+   mutate(chr_ord = row_number()) %>% 
    gather(HAP,CHR,CHR_HAP1,CHR_HAP2) %>%
-   mutate(HAP=as.numeric(gsub("CHR_HAP","",HAP))) %>%
+   mutate(HAP = as.numeric(gsub("CHR_HAP","",HAP))) %>%
    arrange(chr_ord,HAP) %>%
    mutate(chr_hap_ord = row_number()) %>%
    select(CHR,
@@ -317,14 +318,14 @@ rbind(CHR_CONTIG_HAP1_INFO,
          CHR_length,
          contig_order_in_chr,
          new_order_sign)  %>% 
-  spread(contig_order_in_chr,new_order_sign)  %>%
-  left_join(.,CHR_HAP_ORDER) %>%
+  spread(contig_order_in_chr,new_order_sign)  %>% 
+  left_join(.,CHR_HAP_ORDER) %>% 
   arrange(chr_hap_ord,
-          desc(CHR_length))  %>%
+          desc(CHR_length))  %>% 
    select(-HAP,
           -CHR,
           -CHR_length,
-          -chr_hap_ord) %>%
+          -chr_hap_ord) %>% 
    mutate_if(is.numeric, as.character) -> CHR_ORDER_HAP12
  
  rm(ORDER,
@@ -333,7 +334,7 @@ rbind(CHR_CONTIG_HAP1_INFO,
     CHR_LENGTH_HAP12,
     CHR_HAP_ORDER)
  
- bind_rows(mutate_if(setNames(HAP1_HAP2_CONTOGS_NEW_ORDER,c(1,2,3)),is.numeric, as.character),CHR_ORDER_HAP12)  %>%
+ bind_rows(mutate_if(setNames(HAP1_HAP2_CONTOGS_NEW_ORDER,c(1,2,3)),is.numeric, as.character),CHR_ORDER_HAP12)  %>% 
    fwrite(paste0(save_dir,
                  "/",
                  gsub(".paf","",gsub(".*/","",map_hap12)),
