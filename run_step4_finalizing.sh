9331d2f3fb3299f8bdb493bfdc75f1c286bf097a
Mojtaba Jahani <moja987@gmail.com>
2022-09-13 11:37:06 -0700

    added hap2

--- run_step4_finalizing.sh
+++ run_step4_finalizing.sh
@@ -6,6 +6,8 @@ ASSEMBLY_hap2=$3                    #/DATA/home/mjahani/ASSEMBLIES/AGA10/AGA10_A
 RESULT_DIR=$4
 CURATED_ASSEMBLY=$5
 
+CS10_ASSEMBLY=${RESULT_DIR}/CS10_assembly/GCF_900626175.2_cs10_10CHR.fasta
+
 #mix assembly to fasta
 [ -d ${RESULT_DIR}/${SAMPLE}/ASM2FASTA/FINAL_GAP ] || mkdir -p ${RESULT_DIR}/${SAMPLE}/ASM2FASTA/FINAL_GAP
 
@@ -15,7 +17,29 @@ Rscript ${bin}/ASM2FASTA_GAP.R \
     ${RESULT_DIR}/${SAMPLE}/sequences/$(basename ${ASSEMBLY_hap1%%.fasta})_$(basename ${ASSEMBLY_hap2%%.fasta}).fasta \
     ${RESULT_DIR}/${SAMPLE}/ASM2FASTA/FINAL_GAP
 
+GENOME_ASSEMBLY_HAP1=${RESULT_DIR}/${SAMPLE}/ASM2FASTA/FINAL_GAP/${SAMPLE}_FINAL_GAP_hap1.reviewed.chr_assembled.fasta
+GENOME_ASSEMBLY_HAP2=${RESULT_DIR}/${SAMPLE}/ASM2FASTA/FINAL_GAP/${SAMPLE}_FINAL_GAP_hap2.reviewed.chr_assembled.fasta
+#####Hap1
 bash ${bin}/minimap.sh \
-    ${RESULT_DIR}/${SAMPLE}/ASM2FASTA/FINAL_GAP/${SAMPLE}_FINAL_GAP_hap1.reviewed.chr_assembled.fasta \
-    ${RESULT_DIR}/CS10_assembly/GCF_900626175.2_cs10_10CHR.fa \
+    $GENOME_ASSEMBLY_HAP1 \
+    $CS10_ASSEMBLY \
     ${RESULT_DIR}/${SAMPLE}/minimap
+
+[ -d ${RESULT_DIR}/${SAMPLE}/ASM2FASTA/FINAL_ASSEMBLY ] || mkdir -p ${RESULT_DIR}/${SAMPLE}/ASM2FASTA/FINAL_ASSEMBLY
+Rscript ${bin}/CHRID.R \
+    ${RESULT_DIR}/${SAMPLE}/minimap/$(basename "${GENOME_ASSEMBLY_HAP1%%.fasta}")_$(basename "${CS10_ASSEMBLY%%.fasta}").paf \
+    $GENOME_ASSEMBLY_HAP1 \
+    ${SAMPLE}_Hap1 \
+    ${RESULT_DIR}/${SAMPLE}/ASM2FASTA/FINAL_ASSEMBLY
+
+#####Hap2
+bash ${bin}/minimap.sh \
+    $GENOME_ASSEMBLY_HAP2 \
+    $CS10_ASSEMBLY \
+    ${RESULT_DIR}/${SAMPLE}/minimap
+
+Rscript ${bin}/CHRID.R \
+    ${RESULT_DIR}/${SAMPLE}/minimap/$(basename "${GENOME_ASSEMBLY_HAP2%%.fasta}")_$(basename "${CS10_ASSEMBLY%%.fasta}").paf \
+    $GENOME_ASSEMBLY_HAP2 \
+    ${SAMPLE}_Hap2cd mini \
+    ${RESULT_DIR}/${SAMPLE}/ASM2FASTA/FINAL_ASSEMBLY
